de3f36992630a9266344abd4752e54ed0f2b6a4e
Sakshi Chavan <chavansakshi1503@gmail.com>
2025-05-04 12:24:05 +0530

    mySolution.js

---
+++ Day 12 - Hack Santa's Merch Store/mySolution.js
@@ -0,0 +1,7 @@
+// Copy and paste each line below into the textarea as the answer for each task.
+
+// Task 1 
+<button onclick="console.log('You have been hacked 🏴‍☠️')" style="background-color:#31401f;color:white;padding: 10px 10px;">Click me!</button>
+
+// Task 2
+<img src="" onerror="document.querySelector('h2').textContent = 'Do not buy this'">
